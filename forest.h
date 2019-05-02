5e80da11fc66dbdf8c9d70eb77f029d1d7ce6ebc
Sawaryn Melanie <melanie.sawaryn@gmail.com>
2019-05-02 10:49:28 +0200

    Modification of Forest.h
    It define the parameters.

--- forest.h
+++ forest.h
@@ -3,10 +3,16 @@
 #ifndef FOREST
 #define FOREST
 class Forest{
+
+    friend class Tree;
   
   public:
+     // Methods for insertions
+     void PushBack(Tree x); // this method allows us to add a Tree in the at the end. It takes a Tree as parameter.
+
   
   protected:
-  
+    Tree* firstTree_;
+    int nb_elmts_;
 };
 #endif
