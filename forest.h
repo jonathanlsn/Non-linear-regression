358ce343ffd147a3462f12e8fbd5f99bea0bf21f
Sawaryn Melanie <melanie.sawaryn@gmail.com>
2019-05-02 10:54:01 +0200

    Add of constructor
    It create a forest empty.

--- forest.h
+++ forest.h
@@ -10,6 +10,11 @@ class Forest{
      // Methods for insertions
      void PushBack(Tree x); // this method allows us to add a Tree in the at the end. It takes a Tree as parameter.
 
+
+		// Constructors
+    Forest();
+    Forest(const Forest&)=delete;  
+	
   
   protected:
     Tree* firstTree_;
