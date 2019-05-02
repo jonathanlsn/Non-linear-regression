b0bc1d18387034287b90548ff9a507318828cd47
Sawaryn Melanie <melanie.sawaryn@gmail.com>
2019-05-02 10:45:17 +0200

    Add of Pushback to add a Tree
    We can add a tree to the forest at the end

--- forest.cpp
+++ forest.cpp
@@ -1,8 +1,31 @@
 #include "forest.h"
 
 Forest::Forest() {
-  this -> nb_elts_ = 0; 
+  this -> nb_elmts_ = 0; 
   this -> firstTree_ = nullptr;
 }
 
+// insertion at the head of the list
+void Forest::PushBack(Tree x){
+	// creation of the new arbre
+	Tree* nouveau = new Tree(x); 
+	
+    // we have to update the number of elements in the list
+	this -> nb_elmts_ = nb_elmts_ +1;
 
+	// if the list is empty, the head_ takes the value of nouveau
+  if (this -> firstTree_ == nullptr){
+    this -> firstTree_ = nouveau;
+  }
+  
+  else {
+    Tree* dernier = firstTree_;
+    Tree* temp = nullptr;
+    for (int i=1; i<nb_elmts ;++i){
+      temp = dernier -> nextTree_ ;
+      dernier = temp;
+    }
+    dernier -> nextTree_ = nouveau;
+   
+  }
+}
