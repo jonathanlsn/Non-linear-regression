61c7bc8e5bbff7bad9f3b217201cf6c41df285f8
Tanguy <tanguy931@gmail.com>
2019-05-02 11:53:40 +0200

    Adding principals node class characteristics.

--- node.cpp
+++ node.cpp
@@ -1,1 +1,42 @@
 #include "node.h"
+#include <iostream>
+
+//constructor
+Node::Node(std::string str);{
+  values = str;
+  FatherNode_ = nullptr;
+  NextNode1_ = nullptr;
+  Nextnode2_ = nullptr;
+}
+
+Node::Node(std::string str, Node precedent){
+  values_ = str;
+  FatherNode_ = &precedent; 
+  NextNode1_ = nullptr;
+  Nextnode2_ = nullptr;
+
+  if (NextNode1_ == nullptr){
+    precedent.NextNode1_ = &this;
+  }
+  else {
+    precedent.NextNode2_ = &this;
+  }
+
+}
+
+//getter
+Node::std::string values(){
+  return values_;
+}
+
+Node::Node * FatherNode(){
+  return FatherNode;
+}
+
+Node::Node * NextNode1(){
+  return NextNode1_;
+}
+
+Node::Node * NextNode2(){
+  return NextNode2_;
+}
--- node.h
+++ node.h
@@ -1,10 +1,26 @@
+#include "tree.h"
+#include "forest.h"
+#include <iostream>
+
 #ifndef NODE
 #define NODE
 class Node{
   
   public:
+//constructeur
+    Node(std::string str);
+    Node(std::string str,Node precedent);
+
+//getter
+    std::string values();
+    Node * FatherNode();
+    Node * NextNode1();
+    Node * NextNode2();
   
   protected:
-  
+    std::string values_;
+    Node * FatherNode_;
+    Node * NextNode1_;
+    Node * NextNode2_;
 };
 #endif
