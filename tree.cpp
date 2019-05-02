b8bca3ad7f44184bd2346dc4b2d23b9b7e7b88a9
Tanguy <tanguy931@gmail.com>
2019-05-02 10:33:20 +0200

    Adding principals tree class characteristics.

--- tree.cpp
+++ tree.cpp
@@ -1,1 +1,24 @@
 #include "tree.h"
+
+//constructor
+Tree::Tree(Node * node){
+  FirstNode_ = node;
+}
+
+Tree::Tree(Tree tree){
+  
+}
+
+//getter
+Tree::Node FirstNode(){
+  return FirstNode_;
+}
+
+Tree::int Fitness(){
+  return Fitness_;
+}
+
+//setter
+
+
+//method
--- tree.h
+++ tree.h
@@ -1,12 +1,27 @@
 #include "node.h"
+#include "forest.h"
 
 #ifndef TREE
 #define TREE
 class Tree{
   
   public:
+
+//constructor
+    Tree(Node * node);
+    Tree(Tree tree);
+//getter
+    Node FirstNode();
+    int Fitness();
+//setter
   
+//method
+
+
   protected:
+    Node * FirstNode_;
+    int Fitness_;
+    int 
   
 };
 #endif
