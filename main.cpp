3dfa4639b9c9e85a05bcd70e8725ced888aa20b1
baptiste <popotamalberti@gmail.com>
2019-05-06 14:16:46 +0200

    Add corrected random generator

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include "node.h"
 
 #include <iostream>
+#include <time.h> 
 
 int main()
 {
 
   //Creation on an environment of work
-  std::cout<<"Hello World"<<std::endl;
+  //std::cout<<"Hello World"<<std::endl;
   
+  
+  //Générateur aléatoire dépendant du temps (change la seed toute les secondes)
+  srand(time(NULL));
+  int nbgen=rand()%9+1;    //entre 1-9
+  printf("%d\n",nbgen);    //teste affichage
   return 0;
 }
