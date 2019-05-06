d82a2fd00721e612e31bc32ec07bb11067cd5879
baptiste <popotamalberti@gmail.com>
2019-05-06 13:56:58 +0200

    Node.h preparation

--- node.h
+++ node.h
@@ -3,8 +3,14 @@
 class Node{
   
   public:
+  //constructors//
+  Node (int var): //Node variable ou constante
+  Node (int var,Node* next_node1); // Node pour opérateur not
+  Node (int var,Node* next_node1, Node* next_node2); // node pour opérateur
   
   protected:
-  
+  char[] valeur;
+  Node* next_node1;
+  Node* next_node2; 
 };
 #endif
