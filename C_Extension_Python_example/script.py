43ef6c2d77b28775fba8647ef6d75acd7d477b08
Tanguy <tanguy931@gmail.com>
2019-05-13 15:33:45 +0200

    Wrapper is now capcable of creating the two array which contain the information too resolve the problem.

--- C_Extension_Python_example/script.py
+++ C_Extension_Python_example/script.py
@@ -4,7 +4,7 @@ a2 = mwc.generate_A(False)
 mwc.print_A(a1)
 l = [mwc.generate_A(True),mwc.generate_A(False),mwc.generate_A(True)]
 print(l)
-ax,a = mwc.sum_list_As(l)
+ax,a = mwc.sum_list_As([1,0,0])
 print("added A")
 mwc.print_A(ax)
 print(a)
